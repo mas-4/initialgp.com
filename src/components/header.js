d5c984c5b9856e514761e8f260bad48082067f13
malan88 <malan88@standingwater.io>
2019-11-05 17:29:52 -0500

    First steps

--- src/components/header.js
+++ src/components/header.js
@@ -1,42 +1,52 @@
-import { Link } from "gatsby"
-import PropTypes from "prop-types"
+import { useStaticQuery, graphql, Link } from "gatsby"
 import React from "react"
+import styled from "styled-components"
+import logo from "../images/logo.png"
 
-const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
-    </div>
-  </header>
-)
+const Container = styled.header`
+    background-color: white;
+    display: flex;
+    margin: 0.25rem;
+    justify-content: space-around;
+`
+const Nav = styled.nav`
+    a {
+        margin: 0.5rem;
+        text-decoration: none;
+        color: black;
+    }
+`
+const Title = styled.h1`
+    img {
+        vertical-align: top;
+    }
+    a {
+        text-decoration: none;
+        color: black;
+        margin: auto 0.25rem;
+    }
+`
 
-Header.propTypes = {
-  siteTitle: PropTypes.string,
-}
-
-Header.defaultProps = {
-  siteTitle: ``,
+const Header = () => {
+    const data = useStaticQuery(
+        graphql`query { site { siteMetadata { title } } }`
+    )
+    return (
+        <Container>
+            <Title>
+                <Link to="/" >
+                    <img src={logo} alt="Logo" height="40" />
+                </Link>
+                <Link to="/" >{data.site.siteMetadata.title}</Link>
+            </Title>
+            <Nav>
+                <Link to="about">About</Link>
+                <Link to="projects">Projects</Link>
+                <Link to="services">Services</Link>
+                <Link to="contact">Contact</Link>
+            </Nav>
+        </Container>
+    )
 }
 
 export default Header
