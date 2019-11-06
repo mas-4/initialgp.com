9effe0817f273d59ba7575b811bccea34b9ae9c6
malan88 <malan88@standingwater.io>
2019-11-06 14:25:36 -0500

    Indent and stuff layout.js

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,37 +1,32 @@
 import React from "react"
-import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
 
 import Header from "./header"
 import "./layout.css"
 
 const Layout = ({ children }) => {
-  const data = useStaticQuery(graphql`
+    const data = useStaticQuery(graphql`
     query SiteTitleQuery {
-      site {
-        siteMetadata {
-          title
+        site {
+            siteMetadata {
+                title
+            }
         }
-      }
     }
   `)
 
-  return (
-    <>
-      <Header siteTitle={data.site.siteMetadata.title} />
-      <div>
-        <main>{children}</main>
-        <footer>
-          © {new Date().getFullYear()}, Built with
-          {` `}<a href="https://www.gatsbyjs.org">Gatsby</a>
-        </footer>
-      </div>
-    </>
-  )
-}
-
-Layout.propTypes = {
-  children: PropTypes.node.isRequired,
+    return (
+        <>
+            <Header siteTitle={data.site.siteMetadata.title} />
+            <div>
+                <main>{children}</main>
+                <footer>
+                    © {new Date().getFullYear()} Glendale Painting Corporation, Built with
+                        {` `}<a href="https://www.gatsbyjs.org">Gatsby</a>
+                    </footer>
+                </div>
+            </>
+    )
 }
 
 export default Layout
