0066a28005c7bd351724bf5bf606a12d889443a5
malan88 <malan88@standingwater.io>
2019-11-06 11:56:53 -0500

    Initial belleharbor image attempt

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,30 @@
 import React from "react"
-import { Link } from "gatsby"
+import { graphql, Link } from "gatsby"
+import Img from "gatsby-image"
 
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
+const IndexPage = ({ data }) => (
+    <Layout>
+        <SEO title="Home" />
+        <Img
+            fluid={data.file.childImageSharp.fluid}
+        />
+    </Layout>
 )
 
+export const query = graphql`
+    query {
+        file(relativePath: { eq: "parallax.jpg" }) {
+            childImageSharp {
+                fluid {
+                    ...GatsbyImageSharpFluid
+                }
+            }
+        }
+    }
+
+`
 export default IndexPage
