c9a825652cc00f44fbd5ffe69d3bb30e30968b68
malan88 <malan88@standingwater.io>
2019-11-07 12:23:55 -0500

    Create lead about (second item in main)

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,12 +3,14 @@ import Layout from "../components/layout"
 import SEO from "../components/seo"
 import styled from 'styled-components'
 import StyledParallax from "../components/parallax"
+import LeadAbout from "../components/leadabout"
 
 
 const IndexPage = ({ data }) => (
     <Layout>
         <SEO title="Home" />
         <StyledParallax />
+        <LeadAbout />
     </Layout>
 )
 
