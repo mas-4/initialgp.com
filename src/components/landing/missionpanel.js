b8467605e713bde03fe754d2a0d46b33fc6f9aba
malan88 <malan88@standingwater.io>
2019-11-08 15:47:55 -0500

    Rearrange index and add mission panel

---
+++ src/components/landing/missionpanel.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import styled from 'styled-components'
+import Roll from 'react-reveal/Roll'
+
+const Screen = styled.div`
+    padding: 10% 0;
+    color: red;
+    width: 40%;
+    margin-left: 40%;
+`
+const Title = styled.h1`
+    padding-left: 3%;
+    font-size: 500%;
+    line-height: 0.9;
+    border-left 1rem solid black;
+`
+
+const MissionPanel = () => {
+    return (
+        <Screen>
+            <Roll right>
+                <Title>
+                    Take care of the customer and everything else will take care
+                    of itself.
+                </Title>
+            </Roll>
+        </Screen>
+    )
+}
+
+export default MissionPanel
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,15 @@
-import React from "react"
-import Layout from "../components/layout"
-import SEO from "../components/seo"
-import HeroPanel from "../components/landing/heropanel"
-import AboutPanel from "../components/landing/aboutpanel"
+import React from 'react'
+import Layout from '../components/layout'
+import SEO from '../components/seo'
+import HeroPanel from '../components/landing/heropanel'
+import AboutPanel from '../components/landing/aboutpanel'
+import MissionPanel from '../components/landing/missionpanel'
 
 const IndexPage = ({ data }) => (
     <Layout>
-        <SEO title="Home" />
+        <SEO title='Home' />
         <HeroPanel />
+        <MissionPanel />
         <AboutPanel />
     </Layout>
 )
