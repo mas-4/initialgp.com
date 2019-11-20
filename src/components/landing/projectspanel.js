df13939e65d300bcdf7fae2e48a9274cf5911b38
malan88 <malan88@standingwater.io>
2019-11-20 10:44:46 -0500

    Projects Panel title

--- src/components/landing/projectspanel.js
+++ src/components/landing/projectspanel.js
@@ -1,7 +1,22 @@
 import React from 'react'
 import styled from 'styled-components'
 import Img from "gatsby-image"
+import Fade from 'react-reveal/Fade'
 import { useStaticQuery, graphql } from 'gatsby'
+
+const TitlePanel = styled.div`
+    width: 100%;
+    margin: 10rem 0;
+`
+const Title = styled.h1`
+    font-size: 500%;
+    margin: 0 auto;
+    text-align: center;
+    padding: 1.5rem;
+    width: 20%;
+    line-height: 0.8;
+    border: 1rem solid red;
+`
 
 const Panel = styled.div`
     background-color: rgba(41, 41, 41, 0.78);
@@ -53,36 +68,43 @@ const ProjectsPanel = () => {
         }
     `)
     return (
-        <Panel>
-            <Project>
-                <Img fluid={{ ...data.img1.childImageSharp.fluid, aspectRatio: 1.1 }} />
-            </Project>
-            <Project>
-                <Img fluid={{ ...data.img2.childImageSharp.fluid, aspectRatio: 1.1 }} />
-            </Project>
-            <Project>
-                <Img fluid={{ ...data.img3.childImageSharp.fluid, aspectRatio: 1.1 }} />
-            </Project>
-            <Project>
-                <Img fluid={{ ...data.img4.childImageSharp.fluid, aspectRatio: 1.1 }} />
-            </Project>
-            <Project>
-                <Img fluid={{ ...data.img5.childImageSharp.fluid, aspectRatio: 1.1 }} />
-            </Project>
-            <Project>
-                <Img fluid={{ ...data.img6.childImageSharp.fluid, aspectRatio: 1.1 }} />
-            </Project>
-            <Project>
-                <Img fluid={{ ...data.img7.childImageSharp.fluid, aspectRatio: 1.1 }} />
-            </Project>
-            <Project>
-                <Img fluid={{ ...data.img8.childImageSharp.fluid, aspectRatio: 1.1 }} />
-            </Project>
-            <Project>
-                <Img fluid={{ ...data.img9.childImageSharp.fluid, aspectRatio: 1.1 }} />
-            </Project>
-        </Panel>
-    )
-}
+        <>
+            <TitlePanel>
+                <Fade top>
+                    <Title>What We've Done</Title>
+                </Fade>
+            </TitlePanel>
+            <Panel>
+                <Project>
+                    <Img fluid={{ ...data.img1.childImageSharp.fluid, aspectRatio: 1.1 }} />
+                </Project>
+                <Project>
+                    <Img fluid={{ ...data.img2.childImageSharp.fluid, aspectRatio: 1.1 }} />
+                </Project>
+                <Project>
+                    <Img fluid={{ ...data.img3.childImageSharp.fluid, aspectRatio: 1.1 }} />
+                </Project>
+                <Project>
+                    <Img fluid={{ ...data.img4.childImageSharp.fluid, aspectRatio: 1.1 }} />
+                </Project>
+                <Project>
+                    <Img fluid={{ ...data.img5.childImageSharp.fluid, aspectRatio: 1.1 }} />
+                </Project>
+                <Project>
+                    <Img fluid={{ ...data.img6.childImageSharp.fluid, aspectRatio: 1.1 }} />
+                </Project>
+                <Project>
+                    <Img fluid={{ ...data.img7.childImageSharp.fluid, aspectRatio: 1.1 }} />
+                </Project>
+                <Project>
+                    <Img fluid={{ ...data.img8.childImageSharp.fluid, aspectRatio: 1.1 }} />
+                </Project>
+                <Project>
+                    <Img fluid={{ ...data.img9.childImageSharp.fluid, aspectRatio: 1.1 }} />
+                </Project>
+            </Panel>
+        </>
+        )
+    }
 
-export default ProjectsPanel
+    export default ProjectsPanel
